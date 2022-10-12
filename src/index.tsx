b1c3e7946fb98277d199ce5eba3079ce00c0f2e4
QuangHuyBS <huydq@beetsoft.com.vn>
2022-10-13 01:36:08 +0700

    quick fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import { NativeModules, Platform } from 'react-native';
+import * as React from 'react';
+
+import { NativeModules, Platform, StyleSheet, Text, View } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-camera-exam' doesn't seem to be linked. Make sure: \n\n` +
@@ -6,7 +8,7 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
-const CameraExam = NativeModules.CameraExam  ? NativeModules.CameraExam  : new Proxy(
+const CameraExam = NativeModules.CameraExam ? NativeModules.CameraExam  : new Proxy(
       {},
       {
         get() {
@@ -18,3 +20,30 @@ const CameraExam = NativeModules.CameraExam  ? NativeModules.CameraExam  : new P
 export function multiply(a: number, b: number): Promise<number> {
   return CameraExam.multiply(a, b);
 }
+
+export function AppComponent() {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
