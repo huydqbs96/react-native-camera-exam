9d81c3fbf5f483e3d81fd02d9bf41ae318d628bb
QuangHuyBS <huydq@beetsoft.com.vn>
2022-10-12 22:48:07 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CameraExamExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-camera-exam': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
