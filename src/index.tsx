b6a2ae3244eb7ae86c26d4a6c35209fc64dc21b5
SinhNV <sinhnv@beetsoft.com.vn>
2022-11-07 13:24:55 +0700

    [update] view

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-import { StyleSheet, Platform, ViewStyle, AppState } from 'react-native';
+import { StyleSheet, Platform, ViewStyle, AppState, View } from 'react-native';
 import { 
   useCameraDevices, 
   Camera,
@@ -220,7 +220,7 @@ export function CameraView(propCamera: CameraType) {
     }
   };
 
-  if (device == null) return <>viewErrCamera</>;
+  if (device == null) return <>{viewErrCamera}</>;
 
   return (
     <>
