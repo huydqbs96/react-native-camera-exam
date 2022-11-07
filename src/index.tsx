bb78d33b37dc4abe1d98581de29e588b80eebe36
SinhNV <sinhnv@beetsoft.com.vn>
2022-11-07 09:09:10 +0700

    [delete] text import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-import { StyleSheet, Platform, ViewStyle, AppState, Text } from 'react-native';
+import { StyleSheet, Platform, ViewStyle, AppState } from 'react-native';
 import { 
   useCameraDevices, 
   Camera,
